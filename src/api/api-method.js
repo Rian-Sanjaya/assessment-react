6f2b5871cf965fc4caa9c863f5f5d99931127545
Rian Sanjaya <rian.sj@gmail.com>
2023-06-12 00:18:02 +0700

    create table list, add, edit, delete user

--- src/api/api-method.js
+++ src/api/api-method.js
@@ -1,9 +1,5 @@
 import SteinStore from "stein-js-client";
 
 export const steinStore = new SteinStore(
-  "https://api.steinhq.com/v1/storages/632d89aabc148508ba8f0dfb"
-);
-
-export const catSteinStore = new SteinStore(
-  "https://stein.efishery.com/v1/storages/5e1edf521073e315924ceab4/"
+  "https://api.steinhq.com/v1/storages/64859c50d27cdd09f0fa02f6"
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { headerReducer } from "./header";
-import { komoditasReducer } from "./komoditas";
-import { areasReducer } from "./area";
-import { sizesReducer } from "./size";
+import { userReducer } from "./user";
 
 const composeEnhancers = 
   (process.env.NODE_ENV !== 'production' && 
@@ -12,9 +10,7 @@ const composeEnhancers =
 
 const rootReducer = combineReducers({
   header: headerReducer,
-  comodities: komoditasReducer,
-  areas: areasReducer,
-  sizes: sizesReducer,
+  users: userReducer,
 });
 
 export const store = createStore(
